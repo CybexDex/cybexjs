47f66f36d9c92404a4cba275dd03a67251f66e1d
ldwformat <ldwformat@gmail.com>
2018-06-26 13:13:12 +0800

    Add d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,8 @@
+/// <reference types="bigi" />
+import { Point, getCurveByName } from "ecurve";
+import BigInteger from 'bigi';
+
+
 declare module "cybexjs" {
   class Serializer {
     constructor(operation_name: string, types: { [p: string]: any });
@@ -20,18 +25,18 @@ declare module "cybexjs" {
   class Signature {
     constructor(r1, s1, i1);
     static fromBuffer(buf): Signature;
-    toBuffer();
+    toBuffer(): Buffer;
     recoverPublicKeyFromBuffer(buffer);
-    static signBuffer(buf, private_key);
-    static signBufferSha256(buf_sha256, private_key);
-    static sign(string, private_key);
-    verifyBuffer(buf, public_key);
-    verifyHash(hash, public_key);
-    toByteBuffer();
-    static fromHex(hex);
-    toHex();
-    static signHex(hex, private_key);
-    verifyHex(hex, public_key);
+    static signBuffer(buf, private_key): Signature;
+    static signBufferSha256(buf_sha256, private_key): Signature;
+    static sign(string, private_key): Signature;
+    verifyBuffer(buf, public_key): boolean;
+    verifyHash(hash, public_key): boolean;
+    toByteBuffer(): Buffer;
+    static fromHex(hex): Signature;
+    toHex(): string;
+    static signHex(hex, private_key): Signature;
+    verifyHex(hex, public_key): boolean;
   }
   type ParamsOfCheck = {
     accountName: string;
@@ -68,8 +73,61 @@ declare module "cybexjs" {
   const FetchChain: (apiMethod: string, ...args: any[]) => Promise<any>;
   const TransactionHelper: any;
   const Aes: any;
-  const PublicKey: any;
-  const PrivateKey: any;
+  class PublicKey {
+    /** @param {Point} public key */
+    constructor(Q);
+    static fromBinary(bin): PublicKey;
+    static fromBuffer(buffer): PublicKey;
+    /**
+        @arg {string} public_key - like GPHXyz...
+        @arg {string} address_prefix - like GPH
+        @return PublicKey or `null` (if the public_key string is invalid)
+    */
+    static fromPublicKeyString(public_key, address_prefix?): PublicKey | null;
+    static fromStringOrThrow(public_key, address_prefix?): PublicKey | null;
+    toBuffer(compressed?): Buffer;
+    static fromPoint(point): PublicKey;
+    toUncompressed(): PublicKey;
+    /** cyb::blockchain::address (unique but not a full public key) */
+    toBlockchainAddress(): string;
+    /** Alias for {@link toPublicKeyString} */
+    toString(address_prefix?: string): string;
+    /**
+        Full public key
+        {return} string
+    */
+    toPublicKeyString(address_prefix?: string): string;
+    toAddressString(address_prefix?: string): string;
+    toPtsAddy(): string;
+    child(offset): PublicKey;
+    toByteBuffer(): Buffer;
+    static fromHex(hex): PublicKey;
+    toHex(): string;
+    static fromPublicKeyStringHex(hex: string): PublicKey;
+  }
+  class PrivateKey {
+    /**
+        @private see static functions
+        @param {BigInteger}
+    */
+    constructor(d);
+    static fromBuffer(buf: Buffer): PrivateKey;
+
+    /** @arg {string} seed - any length string.  This is private, the same seed produces the same private key every time.  */
+    static fromSeed(seed: string): PrivateKey;
+
+    /** @return {string} Wallet Import Format (still a secret, Not encrypted) */
+    static fromWif(_private_wif: string): PrivateKey;
+    toWif(): string;
+    toPublicKeyPoint();
+    toPublicKey(): PublicKey;
+    toBuffer(): Buffer;
+    toByteBuffer(): Buffer;
+    get_shared_secret(public_key, legacy?: boolean): string;
+    static fromHex(hex: string): PrivateKey;
+    child(offset): PrivateKey;
+    toHex(): string;
+  }
   const ChainTypes: any;
   const ChainValidation: {
     is_account_name: (value, allow_too_short?) => boolean;
